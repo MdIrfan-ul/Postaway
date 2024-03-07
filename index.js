1273cce5d581e68b43fca2f40a0da726bd3530cd
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-08 00:53:40 +0530

    files added successfully

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+
+
+import postRoutes from "./src/features/posts/routes/post.routes.js";
+import userRoutes from "./src/features/users/routes/user.routes.js";
+import jwtAuth from "./src/middlewares/jwtAuth.middleware.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Postaway");
+})
+
+app.use("/api/posts",jwtAuth,postRoutes);
+app.use("/api/user",userRoutes);
+
+export default app;
+
+
+
+
+
