bba2aeded52837dbd3d7d1605447343ef1eeb147
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-09 03:24:39 +0530

    comments models updated

--- src/features/comments/models/comment.model.js
+++ src/features/comments/models/comment.model.js
@@ -1,5 +1,6 @@
 import UserModel from "../../users/models/user.models.js";
-
+import ApplicationError from "../../../middlewares/application.error.middleware.js";
+import PostModel from "../../posts/models/post.model.js";
 export default class CommentModel{
     constructor(id,userId,postId,content){
         this.id = id;
@@ -9,26 +10,55 @@ export default class CommentModel{
     }
     static get(postId){
         const getComments = comments.filter(comment=> comment.postId == postId);
-        return getComments
+        if(getComments.length===0){
+            throw new ApplicationError("No comments for this specific post",404);
+        }
+
+        return getComments;
+        
     }
     static add(userId,postId,content){
+        const post = PostModel.getAll().find(post=>post.id ==postId);
+        if(!post){
+            throw new ApplicationError("post is not found",400);
+        };
+        if(!content){
+            throw new ApplicationError("Content for the post is not present",400);
+        }
         let newComment = new CommentModel(comments.length+1,userId,postId,content);
         comments.push(newComment);
         return newComment;
     }
     static update(id,userId,postId,content){
-        let index = comments.findIndex(comment=>comment.id ==id && comment.postId == postId );
-        if(index==-1){
-            return "Post not found"
-        }else{
-           let updateComment = new CommentModel(id,userId,postId,content);
-           return comments[index]=updateComment;
+        const post = PostModel.getAll().find(post => post.id == postId);
+        if (!post) {
+            throw new ApplicationError("Post not found", 404);
         }
+    
+        // Find the index of the comment to update
+        const index = comments.findIndex(comment => comment.id == id && comment.postId == postId);
+        if (index === -1) {
+            throw new ApplicationError("Comment not found", 404);
+        }
+    
+        // Check if the user is authorized to update the comment
+        if (comments[index].userId !== userId) {
+            throw new ApplicationError("You are not authorized to update this comment", 403);
+        }
+    
+        // Validate content
+        if (!content) {
+            throw new ApplicationError("Content is required for updating the comment", 400);
+        }
+    
+        // Update the comment
+        comments[index].content = content;
+        return comments[index];
     }
     static remove(postId,userId){
         const index = comments.findIndex(comment=>comment.id == postId&& comment.userId==userId);
         if(index==-1){
-            return "comments not found for the post";
+            throw new ApplicationError("comments not found for the post",404);
         }else{
             comments.splice(index,1);
         }
