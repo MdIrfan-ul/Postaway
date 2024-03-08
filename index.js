8d0824f080120523b195bcc036a438837e344366
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-08 17:29:35 +0530

    Comments Feature has been Added successfully

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from "express";
 
 import postRoutes from "./src/features/posts/routes/post.routes.js";
 import userRoutes from "./src/features/users/routes/user.routes.js";
+import commentRoutes from "./src/features/comments/routes/comment.routes.js";
 import jwtAuth from "./src/middlewares/jwtAuth.middleware.js";
 
 const app = express();
@@ -16,6 +17,7 @@ app.get("/",(req,res)=>{
 
 app.use("/api/posts",jwtAuth,postRoutes);
 app.use("/api/user",userRoutes);
+app.use("/api/comments",jwtAuth,commentRoutes);
 
 export default app;
 
