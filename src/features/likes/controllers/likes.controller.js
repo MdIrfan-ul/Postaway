d206546e28a9099689a4f1715518debd88bc1b61
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-09 03:26:00 +0530

    likes controllers updated

--- src/features/likes/controllers/likes.controller.js
+++ src/features/likes/controllers/likes.controller.js
@@ -7,20 +7,21 @@ export default class LikesController{
         const postId = req.params.id;
         const userId = req.userId;
         const newLike = LikeModel.toggle(userId,postId);
-        res.status(201).send(newLike);
+        if(!newLike){
+            res.status(400).status("No Likes for this post")
+        }else{
+
+            res.status(201).json(newLike);
+        }
 
     }
     getAllLikes(req,res,next){
         const postId = req.params.id;
-        try {
-            const likes = LikeModel.get(postId);
-            if (!likes || likes.length === 0) {
-                res.status(404).send("No likes found for the specified post");
-            } else {
+        const likes = LikeModel.get(postId);
+            if (likes && likes.length>0) {
                 res.status(200).json(likes);
+            } else {
+                res.status(404).send("No likes found for the specified post");
             }
-        } catch (error) {
-            res.status(500).send("Internal Server Error");
-        }
     }
 }
