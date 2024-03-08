c9ea8b77f9a279938aed4f420ef0a0b64071997f
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-09 03:27:06 +0530

    likes model updated

--- src/features/likes/models/likes.model.js
+++ src/features/likes/models/likes.model.js
@@ -1,3 +1,5 @@
+import PostModel from "../../posts/models/post.model.js";
+
 export default class LikeModel{
     constructor(id,userId,postId){
         this.id = id;
@@ -6,23 +8,29 @@ export default class LikeModel{
     }
 
     static toggle(userId,postId){
-            const existingLikeIndex = likes.findIndex(like => like.userId == userId && like.postId == postId);
-    
-            if (existingLikeIndex !== -1) {
-                // If like exists, remove it
-                likes.splice(existingLikeIndex, 1);
-                return { liked: false };
-            } else {
-                // If like doesn't exist, add it
-                const newId = likes.length > 0 ? likes[likes.length - 1].id + 1 : 1;
-                const newLike = new LikeModel(newId, userId, postId);
-                likes.push(newLike);
-                return { liked: true };
-            }
+           // Check if the postId exists in the posts collection
+        const existingPost = PostModel.getAll().find(post => post.id == postId);
+        if (!existingPost) {
+            return "Post not found";
+        }
+
+        const existingLikeIndex = likes.findIndex(like => like.userId == userId && like.postId == postId);
+
+        if (existingLikeIndex !== -1) {
+            // If like exists, remove it
+            likes.splice(existingLikeIndex, 1);
+            return { liked: false };
+        } else {
+            // If like doesn't exist, add it
+            const newId = likes.length > 0 ? likes[likes.length - 1].id + 1 : 1;
+            const newLike = new LikeModel(newId, userId, postId);
+            likes.push(newLike);
+            return { liked: true };
+        }
         
     }
    static get(postId){
-        return likes.filter(like=>like.postId ==postId)
+        return  likes.filter(like=>like.postId ==postId);
     }
 }
 
