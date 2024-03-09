5f418d87bc50f97e3268cc2f1ac62b3be28bc32c
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-09 22:12:32 +0530

    features added like filter,save,archieve bookmark

--- src/features/posts/controllers/posts.controller.js
+++ src/features/posts/controllers/posts.controller.js
@@ -1,27 +1,46 @@
+import ApplicationError from "../../../middlewares/application.error.middleware.js";
 import PostModel from "../models/post.model.js";
 
 export default class PostController {
   // Get All Posts
   getPosts(req, res, next) {
-    let posts = PostModel.getAll();
-    res.status(200).send(posts);
+    try {
+      const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
+      const limit = parseInt(req.query.limit) || 10; // Default limit of 10 posts per page
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+
+      const posts = PostModel.getAll();
+      const paginatedPosts = posts.slice(startIndex, endIndex);
+      if(paginatedPosts.length ===0){
+        throw new ApplicationError("No posts available for the specified page",404);
+      }
+
+      res.status(200).json({
+        currentPage: page,
+        totalPages: Math.ceil(posts.length / limit),
+        posts: paginatedPosts
+      });
+    } catch (error) {
+      next(error);
+    }
   }
   // Create Post
   createPost(req, res, next) {
     const userId = req.userId;
     const { caption } = req.body;
-    const imageUrl = req.file.filename
+    const imageUrl = req.file.filename;
     let newPost = PostModel.add(userId, caption, imageUrl);
     res.status(201).send(newPost);
   }
   // Get Specific Post by Id
   getSpecificPost(req, res, next) {
-    const id = req.params.id;
-    let specificPost = PostModel.getById(id);
-    if (specificPost) {
+    try {
+      const id = req.params.id;
+      let specificPost = PostModel.getById(id);
       res.status(200).send(specificPost);
-    } else {
-      res.status(404).send("Post not found");
+    } catch (error) {
+      next(error);
     }
   }
   // Get UserPost By UserId
@@ -31,34 +50,71 @@ export default class PostController {
     res.status(200).send(userPost);
   }
   // Update Post By Id
-  updatePost(req, res) {
+  updatePost(req, res, next) {
     const id = req.params.id;
     const userId = req.userId;
     const { caption, imageUrl } = req.body;
     try {
       const updatedPost = PostModel.update(id, userId, caption, imageUrl);
-      if (!updatedPost) {
-        res.status(404).send("Post not found");
-      } else {
-        res.status(200).json(updatedPost);
-      }
+      res.status(200).json(updatedPost);
     } catch (error) {
-      res.status(500).send("Internal Server Error");
+      next(error);
     }
   }
   // Delete Post By Id
-  deletePost(req, res) {
+  deletePost(req, res, next) {
     const postId = req.params.id;
     const userId = req.userId;
     try {
-      const deletedPost = PostModel.remove(postId, userId);
-      if (deletedPost) {
-        return res.status(404).send("Post not found");
-      } else {
-        return res.status(200).send("Post is Deleted");
-      }
+      PostModel.remove(postId, userId);
+      return res.status(200).send("Post is Deleted");
     } catch (error) {
-      res.status(500).send("Internal Server Error");
+      next(error);
+    }
+  }
+
+  filterPosts(req, res, next) {
+    try {
+      const caption = req.query.caption;
+      console.log("Filtering by caption:", caption);
+      const filteredPosts = PostModel.getByCaption(caption);
+      console.log("Filtered posts:", filteredPosts);
+      res.status(200).json(filteredPosts);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  savePosts(req, res, next) {
+    const postId = req.params.postId;
+    try {
+      const userId = req.userId;
+      PostModel.save(postId, userId);
+      res.status(201).send("Post saved successfully");
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  archivePost(req, res, next) {
+    const postId = req.params.postId;
+    try {
+      PostModel.archivePost(postId);
+      res.status(201).send("Post archived successfully");
+    } catch (error) {
+      next(error);
+    }
+  }
+  
+  bookmarkPost(req,res,next){
+    try {
+      const postId =req.params.postId;
+      const userId = req.userId;
+      PostModel.bookMark(postId,userId);
+      res.status(200).send("Post Bookmarked");
+      
+    } catch (error) {
+      next(error)
     }
   }
 }
