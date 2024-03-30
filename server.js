c74b639e7bbef5aab2a72b407d2ba2d29ba7c050
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-30 07:04:36 +0530

    updated server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from "./index.js";
 import {connectUsingMongoose} from "./src/config/mongoose.db.js";
 
 app.listen(process.env.PORT,(req,res)=>{
-    console.log(`server is listening at localhost:8000`);
+    console.log(`server is listening at localhost:${process.env.PORT}`);
     connectUsingMongoose();
 
 })
