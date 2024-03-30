090cef3c9da8b076692d4029ecd9a801a878054f
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-30 10:56:56 +0530

    multer for storing avatars

--- src/middlewares/file-upload.middleware.js
+++ src/middlewares/file-upload.middleware.js
@@ -3,7 +3,7 @@ import multer from "multer";
 // Multer for storing the user images on the uploads folder locally
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, "./uploads/avatars");
   },
   filename: function (req, file, cb) {
     const fileName =
