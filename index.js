c2459511de97e2eb78f4085df76f94ad3a90aefc
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-30 09:12:55 +0530

    server updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "./env.js"
 import express from "express";
 import jwtAuth from "./src/middlewares/jwtAuth.middleware.js"
 import UserRoutes from "./src/features/users/user.routes.js";
+import ApplicationError from "./src/errorhandlers/application.errors.js";
 
 
 const app = express();
@@ -14,7 +15,7 @@ app.get("/",(req,res)=>{
     res.json({appName:"Postaway-II",msg:"Welcome to the App Express Server"});
 });
 
-app.use("/api/users",UserRoutes);
+app.use("/api/users",UserRoutes)
 // app.use("/api/posts")
 // app.use("/api/comments")
 // app.use("/api/likes")
@@ -22,7 +23,7 @@ app.use("/api/users",UserRoutes);
 // app.use("/api/otp")
 
 app.use((req,res)=>{
-    res.status(404).json(`API Not Found. Please Verify the Document to know More Information at localhost:${process.env.PORT}/api-docs`);
+    res.status(404).json("API Not Found. Please Verify the Document to know More Information at localhost:5000/api-docs");
 })
 
 app.use((err,req,res,next)=>{
