d24757a71a3ae6663fd11ad529774a1fc4eeec38
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-03-31 08:27:09 +0530

    Post Feature Added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import jwtAuth from "./src/middlewares/jwtAuth.middleware.js"
 import UserRoutes from "./src/features/users/user.routes.js";
 import ApplicationError from "./src/errorhandlers/application.errors.js";
+import PostRoutes from "./src/features/posts/post.routes.js";
 
 
 const app = express();
@@ -16,7 +17,7 @@ app.get("/",(req,res)=>{
 });
 
 app.use("/api/users",UserRoutes)
-// app.use("/api/posts")
+app.use("/api/posts",jwtAuth,PostRoutes);
 // app.use("/api/comments")
 // app.use("/api/likes")
 // app.use("/api/friends")
