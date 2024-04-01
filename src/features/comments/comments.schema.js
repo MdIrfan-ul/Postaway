3686b9cbd38b2c8d501e632ee64b5faabd9e84f4
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-04-01 23:08:03 +0530

    schema updated

--- src/features/comments/comments.schema.js
+++ src/features/comments/comments.schema.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const CommentSchema = mongoose.Schema({
-    User:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
-    Post:{type:mongoose.Schema.Types.ObjectId,ref:'post'},
+    User:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    Post:{type:mongoose.Schema.Types.ObjectId,ref:'Post'},
     comment:{type:String,required:[true,"Comment is Required"]}
 
 });
