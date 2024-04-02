1b6ab60d35489e2e6e8a48d1803abc242c870c69
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-04-02 20:10:42 +0530

    friend feature added successfully

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import ApplicationError from "./src/errorhandlers/application.errors.js";
 import PostRoutes from "./src/features/posts/post.routes.js";
 import CommentsRoutes from "./src/features/comments/comments.routes.js";
 import LikeRoutes from "./src/features/likes/likes.routes.js";
+import FriendsRoutes from "./src/features/friends/friends.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -21,7 +22,7 @@ app.use("/api/users", UserRoutes);
 app.use("/api/posts", jwtAuth, PostRoutes);
 app.use("/api/comments", jwtAuth, CommentsRoutes);
 app.use("/api/likes", jwtAuth, LikeRoutes);
-// app.use("/api/friends")
+app.use("/api/friends",jwtAuth,FriendsRoutes);
 // app.use("/api/otp")
 
 app.use((req, res) => {
