ff0391d2a1ceaf5cbf9c46b88cf489688322cd9f
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-04-02 22:40:12 +0530

    otp feature added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import PostRoutes from "./src/features/posts/post.routes.js";
 import CommentsRoutes from "./src/features/comments/comments.routes.js";
 import LikeRoutes from "./src/features/likes/likes.routes.js";
 import FriendsRoutes from "./src/features/friends/friends.routes.js";
+import OtpRoutes from "./src/features/otp/otp.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -23,7 +24,7 @@ app.use("/api/posts", jwtAuth, PostRoutes);
 app.use("/api/comments", jwtAuth, CommentsRoutes);
 app.use("/api/likes", jwtAuth, LikeRoutes);
 app.use("/api/friends",jwtAuth,FriendsRoutes);
-// app.use("/api/otp")
+app.use("/api/otp",jwtAuth,OtpRoutes);
 
 app.use((req, res) => {
   res
